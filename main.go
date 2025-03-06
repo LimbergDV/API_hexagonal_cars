0ca5e8025dc09daa134e4533fcbf3624bc6de16d
LimbergDV <limbergdelvalle@gmail.com>
2025-03-06 02:32:35 -0600

    add: adding main.go

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	routesCars "api-hexagonal-cars/cars/infrastructure/routes"
+	routesCustomers "api-hexagonal-cars/customers/infrastructure/routes"
+	cars "api-hexagonal-cars/cars/infrastructure"
+	customers "api-hexagonal-cars/customers/infrastructure"
+	
+	
+
+	"github.com/gin-gonic/gin"
+)
+
+func main () {
+	cars.GoMySQL()
+	customers.GoMySQL()
+
+	r := gin.Default()
+
+	routesCars.Routes(r)
+	routesCustomers.Routes(r)
+	
+	r.Run(":8080")
+}
