522898c28534f8dadad2f68946c1a4f839e6f0d7
LimbergDV <limbergdelvalle@gmail.com>
2025-03-06 02:12:50 -0600

    add: use cases of customers in application

---
+++ customers/application/DeleteCustomer_useCase.go
@@ -0,0 +1,15 @@
+package application
+
+import "api-hexagonal-cars/customers/domain"
+
+type DeleteCustomer struct{
+	db domain.ICustomer
+}
+
+func NewDeleteCustomer (db domain.ICustomer) *DeleteCustomer {
+	return &DeleteCustomer{db: db}
+}
+
+func (dc *DeleteCustomer) Run (id int) (uint, error) {
+	return dc.db.Delete(id)
+}
---
+++ customers/application/UpdateCustomer_useCase.go
@@ -0,0 +1,15 @@
+package application
+
+import "api-hexagonal-cars/customers/domain"
+
+type UpdateCustomer struct {
+	db domain.ICustomer
+}
+
+func NewUpdateCustomer( db domain.ICustomer) *UpdateCustomer {
+	return &UpdateCustomer{db: db}
+}
+
+func (uc *UpdateCustomer) Run (id int, car domain.Customer) (uint, error) {
+	return uc.db.Update(id, car)
+}
